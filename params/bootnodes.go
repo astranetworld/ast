e79d6a9725f2fea9881128838127787d3d51b931
lucy <40690755+lucyzhang314@users.noreply.github.com>
2024-06-19 14:22:08 -0400

    update formatted output

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -1,4 +1,4 @@
-// Copyright 2023 The astranet Authors
+// Copyright 2024 The astranet Authors
 // This file is part of the astranet library.
 //
 // The astranet library is free software: you can redistribute it and/or modify
