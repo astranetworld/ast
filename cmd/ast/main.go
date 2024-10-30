bfbe94bbcf99b8a8a460c95707d0a64246265349
lucy <40690755+lucyzhang314@users.noreply.github.com>
2024-10-30 16:37:44 -0400

    Update main.go

--- cmd/ast/main.go
+++ cmd/ast/main.go
@@ -1,7 +1,7 @@
 // Copyright 2022 The astranet Authors
 // This file is part of the astranet library.
 //
-// The astranet library is free software: you can redistribute it and/or modify
+// The N42 library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
